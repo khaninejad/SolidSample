61590729d520ba580673eea072f5017875db60b1
Payam Khaninejad <khaninejad@gmail.com>
2020-09-23 13:43:06 +0330

    LSP implementation

---
+++ ArdalisRating/UnknownPolicyRater.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ArdalisRating
+{
+ public   class UnknownPolicyRater:Rater
+    {
+        public UnknownPolicyRater(RatingEngine engine, Logger logger) : base(engine, logger)
+        {
+        }
+
+        public override void Rate(Policy policy)
+        {
+            _logger.Log("Uknown Policy rater");
+        }
+    }
+}
